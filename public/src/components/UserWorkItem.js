92765e708f88b4f5b608168a0b4f5629f6dcc339
yujuan <15809216911@163.com>
2016-10-28 10:47:07 +0800

    Modify the config file

--- public/src/components/UserWorkItem.js
+++ public/src/components/UserWorkItem.js
@@ -1,5 +1,6 @@
 import React ,{Component} from 'react';
 import {Link} from 'react-router';
+//ghjghj
 class UserWorkItem extends Component{
   render(){
     let {id,image,name,date,description} = this.props;
